55bf058b0754140b56bfd1c3c20fb59aaccdee78
Shah Noor <shahnoorkhan9955@gmail.com>
2025-07-28 13:23:50 +0000

    added new files

--- pipelines/feature_pipeline/src/store.py
+++ pipelines/feature_pipeline/src/store.py
@@ -11,7 +11,7 @@ def store_data_to_feature_store():
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"{file_path} does not exist.")
     df = pd.read_csv(file_path)
-    print(f"✅ Loaded data: {df.shape[0]} rows, {df.shape[1]} columns")
+    print(f" Loaded data: {df.shape[0]} rows, {df.shape[1]} columns")
 
     # Load env vars
     project_name = os.getenv("HOPSWORKS_PROJECT_NAME")
@@ -23,7 +23,7 @@ def store_data_to_feature_store():
     # Login
     project = hopsworks.login(project=project_name, api_key_value=api_key)
     feature_store = project.get_feature_store()
-    print("✅ Feature store initialized")
+    print(" Feature store initialized")
 
     # FG config
     fg_name = "sales_record"
@@ -32,10 +32,10 @@ def store_data_to_feature_store():
        # Always recreate feature group cleanly (for now)
     try:
         fg = feature_store.get_feature_group(name=fg_name, version=fg_version)
-        print("⚠️ Deleting existing corrupted feature group")
+        print(" Deleting existing corrupted feature group")
         fg.delete()
     except:
-        print("ℹ️ No existing feature group, continuing")
+        print("ℹ No existing feature group, continuing")
 
     # Recreate it
     fg = feature_store.create_feature_group(
@@ -53,14 +53,14 @@ def store_data_to_feature_store():
     df["created_at"] = pd.to_datetime(df["created_at"], errors='coerce')  # Ensure datetime format
 
     df["product_name"] = df["product_name"].str.slice(0, 100)
-    print("✅ Feature group created cleanly")
+    print(" Feature group created cleanly")
 
 
     # Insert data
     if fg is None:
-        raise RuntimeError("❌ Feature group is None. Aborting.")
+        raise RuntimeError("Feature group is None. Aborting.")
     fg.insert(df, write_options={'start_offline': True, 'wait_for_job': False})
-    print("✅ Data inserted into feature store.")
+    print("Data inserted into feature store.")
 
 if __name__ == "__main__":
     store_data_to_feature_store()
